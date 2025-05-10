00b551817cc6e3e52a21ea28dafd0a8808f504cd
Anonymous9972 <jackryan4487@gmail.com>
2025-05-10 21:25:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = 26741932
-    API_HASH = "d9037cbe01f292eb440468c39c3dc41d"
-    ADMIN_ID = [7047543426]
+    BOT_TOKEN = os.environ.get("7705893334:AAGndCaCXn3lfrqu_TJvMp_A7ULUozXOJ_c")
+    API_ID = 26697299
+    API_HASH = "7326c7f4d7d714d782a130c77b09009c"
+    ADMIN_ID = [5168669934]
     DB_URL = "mongodb+srv://navedmohammad2516:zu02cmOW6medghcJ@cluster0.cq1x5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DB_NAME = os.environ.get("DB_NAME")
-    TXT_LOG = -1002322908140
+    TXT_LOG = -1002261727019
     HOST = "https://www.masterapi.tech"
 
